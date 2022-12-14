31e23a10bd46caf8bde6eadda3133a32c800c2fd
abdul <malik301002@gmail.com>
2022-12-14 15:50:44 +0700

    feat(types): create types file

---
+++ src/types/index.ts
@@ -0,0 +1,26 @@
+interface Owner {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+interface Comment {
+  id: string;
+  content: string;
+  createdAt: Date;
+  owner: Owner;
+  upVotesBy: string[];
+  downVotesBy: string[];
+}
+
+export interface Thread {
+  id: string;
+  title: string;
+  body: string;
+  createdAt: Date;
+  category: string;
+  owner: Owner;
+  comments: Comment[];
+  upVotesBy: string[];
+  downVotesBy: string[];
+}
