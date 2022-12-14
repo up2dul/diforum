aed238d0b6aa8906c1db54a5ab93f44bfd42606c
abdul <malik301002@gmail.com>
2022-12-14 14:35:55 +0700

    style: linting code

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,8 +3,8 @@ module.exports = {
   './src/**/*.{ts,tsx}': () => 'yarn tsc --noEmit',
 
   // Lint & Prettify TS and TSX files
-  './src/**/*.{ts,tsx}': filenames => [
+  './src/**/*.{ts,tsx}': (filenames) => [
     `yarn eslint ${filenames.join(' ')}`,
-    `yarn prettier --write ${filenames.join(' ')} --config ./.prettierrc`
+    `yarn prettier --write ${filenames.join(' ')} --config ./.prettierrc`,
   ],
 };
