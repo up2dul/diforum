7dbd338dca834cf8c3b7cb401d04a22138ce05d8
abdul <malik301002@gmail.com>
2022-12-22 00:40:50 +0700

    feat(comps): create ThreadCard component

---
+++ src/components/ThreadCard.tsx
@@ -0,0 +1,42 @@
+import { IconClock, IconHash, IconMessageDots, IconUser } from '@tabler/icons';
+import { Link } from 'react-router-dom';
+
+import { postedAt } from '@/utils';
+import type { Thread } from '@/types';
+
+type ThreadCardProps = Thread & {
+  author: string;
+};
+
+const ThreadCard = ({
+  title,
+  body,
+  totalComments,
+  author,
+  createdAt,
+  category,
+}: ThreadCardProps) => (
+  <Link to='#'>
+    <article className='thread-card flex flex-col gap-5'>
+      <h2>{title}</h2>
+      <p className='truncate'>{body}</p>
+      <hr className='border-como-300' />
+      <div className='flex flex-wrap items-center justify-between gap-3 text-sm font-medium text-como-600'>
+        <p>
+          <IconMessageDots /> {totalComments}
+        </p>
+        <p>
+          <IconUser /> {author}
+        </p>
+        <p>
+          <IconClock /> {postedAt(createdAt as string)}
+        </p>
+        <p>
+          <IconHash /> {category}
+        </p>
+      </div>
+    </article>
+  </Link>
+);
+
+export default ThreadCard;
