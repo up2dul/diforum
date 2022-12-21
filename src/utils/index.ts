ccad1446b92e095760cd9f6fe687df4ceb8d23b8
abdul <malik301002@gmail.com>
2022-12-22 00:36:37 +0700

    feat(utils): create postedAt function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,27 @@
+function postedAt(date: string) {
+  const now = new Date().getTime();
+  const posted = new Date(date).getTime();
+  const diff = now - posted;
+  const diffDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const diffHours = Math.floor(diff / (1000 * 60 * 60));
+  const diffMinutes = Math.floor(diff / (1000 * 60));
+  const diffSeconds = Math.floor(diff / 1000);
+
+  if (diffDays > 0) {
+    return `${diffDays} days ago`;
+  }
+  if (diffHours > 0) {
+    return `${diffHours} hours ago`;
+  }
+  if (diffMinutes > 0) {
+    return `${diffMinutes} minutes ago`;
+  }
+  if (diffSeconds > 0) {
+    return `${diffSeconds} seconds ago`;
+  }
+  return 'just now';
+}
+
 const menuLinkItems = [
   {
     href: '/leaderboard',
@@ -9,4 +33,4 @@ const menuLinkItems = [
   },
 ];
 
-export { menuLinkItems };
+export { postedAt, menuLinkItems };
