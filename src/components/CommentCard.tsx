559dec6df96ea5c901538c5df267dd3c7650f580
abdul <malik301002@gmail.com>
2022-12-23 02:55:36 +0700

    refactor(comps): edit createdAt position, and resize avatar img

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -6,24 +6,24 @@ import type { Comment as CommentCardProps } from '@/types';
 const CommentCard = ({ owner, createdAt, content, upVotesBy, downVotesBy }: CommentCardProps) => (
   <article className='flex flex-col gap-4 rounded-lg border border-como-600 p-4'>
     <div className='flex items-center gap-3'>
-      <img src={owner.avatar} alt={`${owner.name}'s avatar`} className='w-11 rounded-full' />
+      <img src={owner.avatar} alt={`${owner.name}'s avatar`} className='w-8 rounded-full' />
 
-      <div>
-        <h3>{owner.name}</h3>
-        <p className='text-sm'>{postedAt(createdAt)}</p>
-      </div>
+      <h3>{owner.name}</h3>
     </div>
 
     <p dangerouslySetInnerHTML={{ __html: content }}></p>
 
-    <div className='flex gap-8'>
-      <button type='button'>
-        <IconArrowUp /> {upVotesBy.length}
-      </button>
+    <div className='flex items-center justify-between'>
+      <h3 className='text-sm'>{postedAt(createdAt)}</h3>
+      <div className='flex gap-8'>
+        <button type='button'>
+          <IconArrowUp /> {upVotesBy.length}
+        </button>
 
-      <button type='button'>
-        <IconArrowDown /> {downVotesBy.length}
-      </button>
+        <button type='button'>
+          <IconArrowDown /> {downVotesBy.length}
+        </button>
+      </div>
     </div>
   </article>
 );
