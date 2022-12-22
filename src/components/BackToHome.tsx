c32c5e40e56e72b75c0d86869f1d308b0b6a8bba
abdul <malik301002@gmail.com>
2022-12-23 02:37:14 +0700

    feat: text-link class

--- src/components/BackToHome.tsx
+++ src/components/BackToHome.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import { IconArrowBackUp } from '@tabler/icons';
 
 const BackToHome = () => (
-  <Link to='/' className='mb-5 inline-block text-green-700 hover:underline'>
+  <Link to='/' className='text-link mb-5 inline-block'>
     <IconArrowBackUp /> Back to home
   </Link>
 );
