9a7b316a01a35f0dc2421b25ba13dcfad8756746
abdul <malik301002@gmail.com>
2022-12-23 02:42:42 +0700

    feat(comps): create CommentCard component

---
+++ src/components/CommentCard.tsx
@@ -0,0 +1,31 @@
+import { IconArrowDown, IconArrowUp } from '@tabler/icons';
+
+import { postedAt } from '@/utils';
+import type { Comment as CommentCardProps } from '@/types';
+
+const CommentCard = ({ owner, createdAt, content, upVotesBy, downVotesBy }: CommentCardProps) => (
+  <article className='flex flex-col gap-4 rounded-lg border border-como-600 p-4'>
+    <div className='flex items-center gap-3'>
+      <img src={owner.avatar} alt={`${owner.name}'s avatar`} className='w-11 rounded-full' />
+
+      <div>
+        <h3>{owner.name}</h3>
+        <p className='text-sm'>{postedAt(createdAt)}</p>
+      </div>
+    </div>
+
+    <p dangerouslySetInnerHTML={{ __html: content }}></p>
+
+    <div className='flex gap-8'>
+      <button type='button'>
+        <IconArrowUp /> {upVotesBy.length}
+      </button>
+
+      <button type='button'>
+        <IconArrowDown /> {downVotesBy.length}
+      </button>
+    </div>
+  </article>
+);
+
+export default CommentCard;
