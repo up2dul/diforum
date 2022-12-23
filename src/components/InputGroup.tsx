29609921030606665cd46d1e9fdb5dd4dffeee89
abdul <malik301002@gmail.com>
2022-12-24 00:09:23 +0700

    feat(comps): add minLength props

--- src/components/InputGroup.tsx
+++ src/components/InputGroup.tsx
@@ -1,15 +1,17 @@
 type InputGroupProps = {
   type?: 'text' | 'email' | 'password';
+  minLength?: number;
   placeholder: string;
   children: string;
 };
 
-const InputGroup = ({ type = 'text', placeholder, children }: InputGroupProps) => (
+const InputGroup = ({ type = 'text', minLength = 3, placeholder, children }: InputGroupProps) => (
   <label>
     {children}
     <input
       type={type}
       className='mt-1'
+      minLength={minLength}
       maxLength={30}
       placeholder={placeholder}
       autoComplete='off'
