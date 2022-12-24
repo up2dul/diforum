532575e201424d431ae96ea7bf673c417942aeac
abdul <malik301002@gmail.com>
2022-12-25 02:03:29 +0700

    refactor(pages): react-hook-form implementation for Register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,44 +1,77 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import type { SubmitHandler } from 'react-hook-form';
 
+import { asyncPreloadProcess } from '@/store/slice/is-preload';
+import { asyncRegisterUser } from '@/store/slice/users';
 import BackToHome from '@/components/BackToHome';
 import Button from '@/components/Button';
 import InputGroup from '@/components/InputGroup';
 import RegisterSvg from '@/assets/register.svg';
+import type { AppDispatch } from '@/store';
 
-const Register = () => (
-  <>
-    <BackToHome />
-    <h2>📲 Register for your account</h2>
+type Inputs = {
+  fullName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 
-    <section className='mt-10 flex justify-center lg:justify-between'>
-      <form className='flex flex-col gap-5'>
-        <p>
-          Already have an account? Log in{' '}
-          <Link to='/login' className='text-link'>
-            here
-          </Link>
-        </p>
+const Register = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { register, handleSubmit } = useForm<Inputs>();
 
-        <InputGroup placeholder='e.g. John Doe'>Full name</InputGroup>
+  useEffect(() => {
+    dispatch(asyncPreloadProcess());
+  }, [dispatch]);
 
-        <InputGroup type='email' placeholder='e.g. john@gmail.com'>
-          Email
-        </InputGroup>
+  const onSubmit: SubmitHandler<Inputs> = ({ fullName, email, password }) => {
+    dispatch(asyncRegisterUser({ name: fullName, email, password }));
+  };
 
-        <InputGroup type='password' minLength={6} placeholder='min. 6 characters'>
-          Password
-        </InputGroup>
+  return (
+    <>
+      <BackToHome />
+      <h2>📲 Register for your account</h2>
 
-        <InputGroup type='password' minLength={6} placeholder='same as Password input'>
-          Confirm password
-        </InputGroup>
+      <section className='mt-10 flex justify-center lg:justify-between'>
+        <form className='flex flex-col gap-5' onSubmit={handleSubmit(onSubmit)}>
+          <p>
+            Already have an account? Log in{' '}
+            <Link to='/login' className='text-link'>
+              here
+            </Link>
+          </p>
 
-        <Button isSubmit>Register</Button>
-      </form>
+          <InputGroup placeholder='e.g. John Doe' {...register('fullName')}>
+            Full name
+          </InputGroup>
 
-      <img src={RegisterSvg} alt='Register illustration' className='hidden w-[350px] lg:block' />
-    </section>
-  </>
-);
+          <InputGroup placeholder='e.g. john@gmail.com' {...register('email')}>
+            Email
+          </InputGroup>
+
+          <InputGroup type='password' placeholder='min. 6 characters' {...register('password')}>
+            Password
+          </InputGroup>
+
+          <InputGroup
+            type='password'
+            placeholder='min. 6 characters'
+            {...register('confirmPassword')}
+          >
+            Confirm password
+          </InputGroup>
+
+          <Button isSubmit>Register</Button>
+        </form>
+
+        <img src={RegisterSvg} alt='Register illustration' className='hidden w-[350px] lg:block' />
+      </section>
+    </>
+  );
+};
 
 export default Register;
