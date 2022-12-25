1dab37782fe16e1b88db2d6c0f2383116f1e6d77
abdul <malik301002@gmail.com>
2022-12-25 19:29:51 +0700

    refactor(utils): fix typo and edit password validation
    edit password in registerSchema to be must contain letters and numbers

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,11 +1,13 @@
 import * as yup from 'yup';
 
+const lettersAndNumbers = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
+
 const loginSchema = yup.object({
   email: yup
     .string()
     .email('Must be a valid email')
-    .max(30, 'Max, 30 characters')
-    .required('Enter your email address'),
+    .required('Enter your email address')
+    .max(30, 'Max, 30 characters'),
   password: yup
     .string()
     .required('Enter your password')
@@ -14,11 +16,12 @@ const loginSchema = yup.object({
 });
 
 const registerSchema = yup.object({
-  fullName: yup.string().max(32, 'Max, 32 characters').required('Enter your email address'),
+  fullName: yup.string().required('Enter your name').max(32, 'Max, 32 characters'),
   email: yup.string().email('Must be a valid email').required('Enter your email address'),
   password: yup
     .string()
     .required('Enter your password')
+    .matches(lettersAndNumbers, 'Password must contain letters and numbers')
     .min(6, 'Min. 6 characters')
     .max(30, 'Max. 30 characters'),
   confirmPassword: yup
