cb6f939d5c6290017c27d999e6c8a34b1c4e795f
abdul <malik301002@gmail.com>
2022-12-31 14:30:57 +0700

    feat(comps): add Toaster from react-hot-toast

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,5 @@
+import { Toaster } from 'react-hot-toast';
+
 import Navbar from './Navbar';
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
@@ -5,6 +7,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => (
     <Navbar />
 
     <main className='x-container mt-[70px] pt-10 pb-16'>{children}</main>
+
+    <Toaster position='bottom-right' toastOptions={{ className: 'bg-green-50' }} />
   </>
 );
 
