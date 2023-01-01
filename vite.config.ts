fd8c91b4d9ede61e3ef4e196258029341fb2cd9c
abdul <malik301002@gmail.com>
2023-01-01 10:58:48 +0700

    chore: edit test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -8,4 +9,8 @@ export default defineConfig({
     alias: [{ find: '@', replacement: path.resolve(__dirname, '/src') }],
   },
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 });
