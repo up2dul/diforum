b04a97b460a2521f4f666485bdb90c66a604af26
abdul <malik301002@gmail.com>
2023-02-06 20:27:20 +0700

    refactor(pages): show toast on error instead of success login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,5 @@
-import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Helmet } from 'react-helmet-async';
 import toast from 'react-hot-toast';
 import type { SubmitHandler } from 'react-hook-form';
@@ -6,15 +7,21 @@ import type { SubmitHandler } from 'react-hook-form';
 import { asyncSetAuthUser } from '@/store/slice/auth-user';
 import LoginForm from '@/components/form/LoginForm';
 import LoginSvg from '@/assets/login.svg';
-import type { AppDispatch } from '@/store';
+import type { AppDispatch, RootState } from '@/store';
 import type { LoginInputs } from '@/types';
 
 const Login = () => {
+  const { error } = useSelector((state: RootState) => state.authUser);
   const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+    }
+  }, [error]);
 
   const handleSubmit: SubmitHandler<LoginInputs> = (data) => {
     dispatch(asyncSetAuthUser(data));
-    toast.success('Log in successfully');
   };
 
   return (
